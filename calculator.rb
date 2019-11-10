96134f5c22a6eaf5b922033cd04b351e51a60572
bright-format-7059 <v.shengeliya@gmail.com>
2019-11-10 23:52:54 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,5 +4,6 @@ second_number== 2
 sum_var= frist_number+ second_number
 
 dif_var= first_number-second_number
-product_var = fir
+product_var = first_number*second_number
+quotient= first_number/second_number
 
