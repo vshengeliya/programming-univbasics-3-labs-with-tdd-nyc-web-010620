daa2f8d099e3eb11b6a1c26796bb88221c1cdbd7
bright-format-7059 <v.shengeliya@gmail.com>
2019-11-10 23:54:12 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,9 @@
 first_number == 5
 second_number== 2
 
-sum_var= frist_number+ second_number
+sum= frist_number+ second_number
 
-dif_var= first_number-second_number
-product_var = first_number*second_number
+difference= first_number-second_number
+product= first_number*second_number
 quotient= first_number/second_number
 
