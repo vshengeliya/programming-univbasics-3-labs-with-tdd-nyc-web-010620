07db8330e0cc1c4b32720cf514a921c92b79d687
bright-format-7059 <v.shengeliya@gmail.com>
2019-11-10 23:50:50 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,8 @@
-first_number = 5
-second_number= 2
+first_number == 5
+second_number== 2
 
 sum_var= frist_number+ second_number
 
 dif_var= first_number-second_number
-product_var =
+product_var = fir
 
