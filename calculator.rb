ce82a27e14c9ce482deeea0bfb42d9d5b03687d6
bright-format-7059 <v.shengeliya@gmail.com>
2019-11-11 00:07:28 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
-# Add your variables here
+first_number = 5.0
+second_number= 2.0
+
+sum = first_number+ second_number
+difference = first_number- second_number
+product = first_number* second_number
+quotient = first_number/ second_number
